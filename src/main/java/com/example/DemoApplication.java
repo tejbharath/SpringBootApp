a9fa469ca2191c9b90ff4fe318fc9b4535f3b355
Tej Bharath Mujje <tmujje@gmail.com>
2017-02-27 20:34:06 -0600

    Added a New comment

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
-
 @SpringBootApplication
-
 public class DemoApplication {
 
+	// Entry point of the spring boot application
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
